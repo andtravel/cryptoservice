097fab93f34539e43d6b740647be3f55146f92ac
andrey <anovts@gmail.com>
2024-01-07 23:13:51 +0200

    Create Mail Service for scheduling daily mailing

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,8 @@
 <?php
 
-<<<<<<< HEAD
-use App\Http\Controllers\ChartController;
-use App\Http\Controllers\HomeController;
-=======
+
 use App\Http\Controllers\HomeController;
 use App\Models\User;
->>>>>>> feature_mail_service
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -31,12 +27,9 @@ Auth::routes();
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/choice', [HomeController::class, 'choosePage'])->name('page');
 Route::post('/choice', [HomeController::class, 'chooseCrypto'])->name('cryptos');
-<<<<<<< HEAD
-Route::get('/chart/data', [ChartController::class, 'chartData']);
-=======
 Route::get('/chart/data', [HomeController::class, 'chartData'])->name('chartData');
 Route::get('/mail/{id}', function ($id) {
             $user = User::findOrFail($id);
     return new App\Mail\Mailing($user);
 });
->>>>>>> feature_mail_service
+
